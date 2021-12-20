2ae2eb6c50b9cfe09ef37c29c285dcf3fac8a607
richard ingwe <richardingwe5@gmail.com>
2021-12-20 22:22:02 +0100

    inital commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+// import './random_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primaryColor: Colors.purple[900]),
+        home: Scaffold(
+          appBar: AppBar(title: Text('WordPair Generator')),
+        ));
+  }
+}
