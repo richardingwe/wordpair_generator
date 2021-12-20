a9c3cc895c34a2161e134ea4af3a7bd57f95978f
richard ingwe <richardingwe5@gmail.com>
2021-12-20 23:01:14 +0100

    added listview widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,42 @@ class MyApp extends StatelessWidget {
     final wordPair = WordPair.random();
     return MaterialApp(
         theme: ThemeData(primaryColor: Colors.purple[900]),
-        home: Scaffold(
-          appBar: AppBar(title: Text('WordPair Generator')),
-          body: Center(
-            child: Text(wordPair.asPascalCase),
-          ),
-        ));
+        home: RandomWords());
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  Widget _buildList() {
+    return ListView(
+      padding: const EdgeInsets.all(8),
+      children: <Widget>[
+        Container(
+          height: 50,
+          color: Colors.amber[600],
+          child: const Center(child: Text('Entry A'),),
+        ),
+        Container(
+          height: 50,
+          color: Colors.amber[500],
+          child: const Center(child: Text('Entry B'),),
+        ),
+        Container(
+          height: 50,
+          color: Colors.amber[400],
+          child: const Center(child: Text('Entry C'),),
+        ),
+      ],
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('WordPair - Generator')),
+      body: _buildList());
   }
 }
